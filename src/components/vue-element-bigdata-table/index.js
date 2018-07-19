00449942cc7b8fe5f33e2caf42111aa7b6b45bbc
308822989@qq.com <308822989@qq.com>
2018-07-19 18:51:43 +0800

    del

--- src/components/vue-element-bigdata-table/index.js
+++
@@ -1,8 +0,0 @@
-import ElBigdataTable from './table.vue';
-const install = (Vue, opts = {}) => {
-  Vue.component('ElBigdataTable', ElBigdataTable);
-};
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
-export default Object.assign(ElBigdataTable, {install});
